e909bcadcf637c1f3ae85d3ae51fb082ad703f22
吴耀洪 <wuyaohong@cony.local>
2016-08-01 21:13:34 +0800

    Initial Commit

---
+++ MiniPinterest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MiniPinterest
+//
+//  Created by 吴耀洪 on 16/8/1.
+//  Copyright © 2016年 PinterestLittleTiger. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MiniPinterest/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MiniPinterest
+//
+//  Created by 吴耀洪 on 16/8/1.
+//  Copyright © 2016年 PinterestLittleTiger. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
